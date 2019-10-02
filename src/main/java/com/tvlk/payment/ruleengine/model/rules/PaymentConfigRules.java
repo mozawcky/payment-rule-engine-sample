8778ffaeae8b1a331e4325b26f8571afbe0b1533
dennis.dao <dennis.dao@traveloka.com>
2019-10-02 16:10:41 +0800

    re-order the generated rules

---
+++ src/main/java/com/tvlk/payment/ruleengine/model/rules/PaymentConfigRules.java
@@ -0,0 +1,17 @@
+package com.tvlk.payment.ruleengine.model.rules;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+public class PaymentConfigRules {
+  String id;
+  int priority;
+  List<RuleDetails> ruleDetails;
+}
