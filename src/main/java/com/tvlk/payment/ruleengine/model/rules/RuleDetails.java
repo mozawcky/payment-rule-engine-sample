16493faadadd5bb053897a79f4c6dd8587fd3772
zaky <zaky.abdulrazaq@traveloka.com>
2019-10-03 13:12:43 +0700

    add converter for rule details

--- src/main/java/com/tvlk/payment/ruleengine/model/rules/RuleDetails.java
+++ src/main/java/com/tvlk/payment/ruleengine/model/rules/RuleDetails.java
@@ -1,5 +1,6 @@
 package com.tvlk.payment.ruleengine.model.rules;
 
+import com.tvlk.payment.ruleengine.groovy.GroovyRule;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,30 @@ public class RuleDetails {
   private String name;
   private String description;
   private String field;
-  private String operator;
+  private RuleOperatorEnum operator;
   private Object value;
+
+  public GroovyRule toGroovy() {
+
+    String condition = "";
+    switch (operator) {
+      case EQUAL:
+        condition = field + ".equals(" + "'" + value + "')";
+        break;
+      case IN:
+        condition = value + ".contains(" + field + "')";
+        break;
+      case LTE:
+        condition = field + "<=" + value;
+        break;
+      case GTE:
+        condition = field + ">=" + value;
+        break;
+    }
+
+    return new GroovyRule()
+        .name(name)
+        .description(description)
+        .when(condition);
+  }
 }
